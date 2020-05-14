06fabc3856664decd7fb8edf4565275b6b34815e
Pierre sarazin <pierre.sarazin91@yahoo.fr>
2020-05-14 14:12:38 +0200

    update

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  resources :cocktails do
+    resources :ingrdients, only: [:new, :create]
+  end
+
+  resources :ingredients, only: [:destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
